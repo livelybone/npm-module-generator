3e8127de49c4b7f678fe5ac25db829b810ce4ca0
livelybone <2631541504@qq.com>
2018-08-26 21:21:21 +0800

    edit

--- k/test/index.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import Index from '../src/components/Index.vue';
-
-describe('Index.vue', () => {
-  it('Rendered', () => {
-    const wrapper = shallowMount(Index);
-    expect(wrapper.find('div').exists()).to.equal(true);
-  });
-});
